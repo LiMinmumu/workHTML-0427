793b70d72d2e493f87fb75e714a157cd22cd15fd
UniqueJoker11 <yongyuandexiaochou@126.com>
2016-03-23 23:36:40 +0800

    整理网站内容

--- app.js
+++ app.js
@@ -8,12 +8,12 @@ var ejs = require('ejs');
 //获取express框架
 var app = express();
 
-// view engine setup
+// 设定页面及其解析引擎
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -22,22 +22,24 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.engine('.html', ejs.__express);
 app.set('view engine', 'html');
 
-//定义访问路由
-var routes = require('./routes/index');
-var users = require('./routes/users');
-
+//应用路由
+var routes = require('./routes/controller/index');
+//配置访问路由
 app.use('/', routes);
-app.use('/users', users);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+  var err = new Error('当前页面找不到');
   err.status = 404;
   next(err);
 });
 
-// error handlers
-
+//错误处理器
+app.use(function (err,req,res,next) {
+  console.error(err.stack);
+  res.status(500).send('服务器出现问题了,请稍后再试!');
+})
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
@@ -49,16 +51,5 @@ if (app.get('env') === 'development') {
     });
   });
 }
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
-
 
 module.exports = app;
--- routes/controller/index.js
+++ routes/controller/index.js
@@ -1,15 +1,20 @@
 var express = require('express');
+
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
 router.get("/detail",function(req,res,next){
   res.render("detail",{title:"Detail"});
 });
 
 router.post("/list",function(req,res,next){
   res.json({ user: 'tobi' }).end();
+});
+router.get("/minmin",function(req,res,next){
+    res.sendFile("abc.txt",{"root":"../views"});
 });
 module.exports = router;
